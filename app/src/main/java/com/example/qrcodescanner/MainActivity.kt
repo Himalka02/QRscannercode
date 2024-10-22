c1b60e701d8bb9a2d64429cddfe36764bb9e4602
Himalka02 <himalkakaushan6@gmail.com>
2024-10-22 21:33:13 +0530

    final commit

---
+++ app/src/main/java/com/example/qrcodescanner/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.qrcodescanner
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.zxing.integration.android.IntentIntegrator
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val buttonScan = findViewById<Button>(R.id.button_scan)
+        val textViewResult = findViewById<TextView>(R.id.textView_result)
+        // Set button click listener to initiate scan
+        buttonScan.setOnClickListener {
+            val integrator = IntentIntegrator(this)
+            integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES)
+            integrator.setPrompt("Scan a barcode or QR code")
+            integrator.setCameraId(0) // Use a specific camera of the device
+            integrator.setBeepEnabled(true)
+            integrator.setBarcodeImageEnabled(true)
+            integrator.initiateScan()
+        }
+    }
+    // Handle scan result
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            if (result.contents == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+            } else {
+                val textViewResult = findViewById<TextView>(R.id.textView_result)
+                textViewResult.text = "Scanned result: ${result.contents}"
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+}
